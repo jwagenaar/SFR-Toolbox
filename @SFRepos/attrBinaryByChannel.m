c2775b17468fbaed681ec5c4a31efd982f193d10
Joost Wagenaar <git@jwagenaar.net>
2012-06-09 14:01:54 -0400

    doc update

--- @SFRepos/attrBinaryByChannel.m
+++ @SFRepos/attrBinaryByChannel.m
@@ -1,13 +1,5 @@
 function attr = attrBinaryByChannel(obj, attr)
   % BINARYBYCHANNEL  See INFOBINARYBYCHANNEL
-
-  % Copyright (c) 2012, J.B.Wagenaar
-  % This source file is subject to version 3 of the GPL license, 
-  % that is bundled with this package in the file LICENSE, and is 
-  % available online at http://www.gnu.org/licenses/gpl.txt
-  %
-  % This source file can be linked to GPL-incompatible facilities, 
-  % produced or made available by MathWorks, Inc.
   
-  % Not implemented yet.
+  % There are not attributes that are embedded in the files for this format.  
 end
