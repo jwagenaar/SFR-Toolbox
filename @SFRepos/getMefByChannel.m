37362306d49dd39fa044cc3e40ce8398ef7fe82b
Joost Wagenaar <git@jwagenaar.net>
2012-06-14 17:18:04 -0400

    quick fix

--- @SFRepos/getMefByChannel.m
+++ @SFRepos/getMefByChannel.m
@@ -23,11 +23,46 @@ function data = getMefByChannel( obj, channels, indeces, filePath, ~)
   assert(lIndeces == (indeces(lIndeces)-indeces(1)+1), 'SciFileRepos:getMEF',...
     'The GETMEF method only supports sorted continuous indeces.');
   
+  getIndexArray = true;
+  if ~isempty(obj.userData);
+    getIndexArray = false;
+  end
+  
   data = zeros(length(indeces), length(channels));
   for iChan = 1:length(channels)
     % Get information from mef header and index
     fileName      = fullfile(filePath, obj.files{channels(iChan)});
-    data(:,iChan) = decomp_mef(fileName, indeces(1), indeces(lIndeces), '');            
+    
+    if getIndexArray
+      
+      try
+      % Start Timer for showing progress for reading header.
+      % Init Timer
+      display('Indexing MEF file.');
+      progress_timer = timer;
+      set(progress_timer, 'ExecutionMode', 'FixedRate');
+      set(progress_timer, 'BusyMode','queue');
+      set(progress_timer, 'Period', 1);
+      set(progress_timer, 'StartDelay',1);
+      set(progress_timer, 'TimerFcn', @(x,y)fprintf('.'));
+      start(progress_timer);
+      
+%       [data(:, iChan) obj.userData] = ...
+%         decomp_mef(fileName, indeces(1), indeces(lIndeces), '');
+      data(:,iChan) = decomp_mef(fileName, indeces(1), indeces(lIndeces), '');
+      stop(progress_timer);
+     
+      delete(progress_timer);
+      fprintf('\nFile Indexing complete.');
+      getIndexArray = false;
+      catch ME
+        delete(progress_timer);
+        rethrow(ME);
+      end
+      
+    else
+      data(:,iChan) = decomp_mef(fileName, indeces(1), indeces(lIndeces), '');  
+    end
   end
 
 
