6a746fb2d6e2f2a6a6e15d86561e2c0fd493d286
Joost Wagenaar <git@jwagenaar.net>
2012-09-06 14:11:10 -0400

    updated template

--- Templates/infoTemplate.m
+++ Templates/infoTemplate.m
@@ -2,13 +2,19 @@ function out = infoTemplate(obj, locPath, option)
   %INFOTEMPLATE  Short one-sentence summary of file-type.
   %
   %   Additional information.
-    
+  
   assert(nargin == 3, 'SciFileRepos:infoMethod', ...
     'Incorrect number of input arguments for infoMethod.');
   
+  
+  % -- Set the required and optional getOptions here --
+  requiredAttr = {};
+  optionalAttr = {};
+  
+  
   switch option
     case 'init'
-      % Required output structure for case 'init'.
+      % -- -- Don't change this section -- -- 
       out = struct(...
         'requiredAttr', [], ...
         'optionalAttr', [], ...
@@ -16,14 +22,33 @@ function out = infoTemplate(obj, locPath, option)
         'format', [] ...
         );
 
-      % -- Set the contents of the out structure here --
+      out.requiredAttr = requiredAttr; 
+      out.optionalAttr = optionalAttr; 
+      % -- -- -- -- -- -- -- -- -- -- -- -- -
+
+      % Set the contents of the out structure here --
       % ADD CONTENT
       
     case 'info'
       out = struct([]);       
-      % -- Set the contents of the out structure here --
-      % ADD CONTENT
       
+      % -- Set the contents of the out structure here --
+      % 
+      % Here, you should include code that reads out the header information of
+      % the raw data-files.
+      %
+      % ADD CONTENT
+    
+    case 'attr'
+      % -- -- Don't change this section -- -- 
+      % It is used to return the getOptions in various
+      % internal SFR-methods.
+      out = struct(...
+        'reqAttr', [], ...
+        'optAttr', []);
+      out.reqAttr = requiredAttr;
+      out.optAttr = optionalAttr;  
+      % -- -- -- -- -- -- -- -- -- -- -- -- -
     otherwise
       error('SciFileRepos:getattr','Incorrect option: %s', option);
   end
